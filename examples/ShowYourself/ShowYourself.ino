e1a583418cdf05db3e547a211ebf21d3b6470c1e
Crocus726 <crocus726@gmail.com>
2022-05-19 20:11:18 +0900

    Update ShowYourself.ino

--- examples/ShowYourself/ShowYourself.ino
+++ examples/ShowYourself/ShowYourself.ino
@@ -26,26 +26,26 @@ Fs4, Cs5, Cs5, Fs4, Fs4, R, Gs4, As4, B4, As4, Gs4, Fs4, Cs5, B4, As4, Gs4, //5 
 Cs5, Fs5, Es5, Cs5, Cs5, Ds5, Es5, Gs4, R, Es5, Es5, Fs5, Ds5, Cs5, Ds5, //6 I have always been a fortress, cold secrets deep inside
 A4, Cs5, Ds5, E5, E5, R, A4, E5, Fs5, Fs5, Gs5, Gs5, //7 You have secrest too, but you don't have to hide
 Gs5, Fs5, Fs5, R, Fs5, Fs5, Ds5, Cs5, Cs5, Es5, //8 Show yourself, I'm dying to meet you
-Gs5, Fs5, Fs5, R, Fs5, Ds5, Cs5, E4, R, //9 Show yourself, it's you're turn
+Gs5, Fs5, Fs5, R, Fs5, Ds5, Cs5, Gs4, R, //9 Show yourself, it's you're turn
 As4, Gs4, As4, Cs5, As4, Cs5, Ds5, Cs5, As4, Fs4, Gs4, As4, Cs5, Fs5, Es5, Gs5, //10 Are you the one I've been looking for all of my life?
-B5, As5, As5, Gs5, Fs5, R, Fs5, Es5, Es5, Fs5, E5, //11 Show yourself, I'm ready to learn
-R, Fs5, F5, G5, Ds5, R, G5, A5, G5, Fs5, C5, R, //12 Ah-ah, ah-ah (Ah-ah ah-ah-ah)
+B5, As5, As5, Gs5, Fs5, R, Fs5, Es5, Es5, Fs5, Ef5, //11 Show yourself, I'm ready to learn
+R, Fs5, F5, Fs5, Ds5, R, G5, A5, G5, Fs5, E5, R, //12 Ah-ah, ah-ah (Ah-ah ah-ah-ah)
 G4, G4, G4, G4, A4, D5, B4, D5, D5, D5, C5, B4, D5, B4, E5, //13 I've never felt so certain, all my life I've been torn
 D5, D5, D5, D5, E5, E5, D5, D5, D5, D5, D5, D5, C5, B4, D5, G5, G5, //14 But I'm here for a reason, could it be the reason I was born?
 G5, G5, A5, Fs5, D5, E5, Fs5, A4, Fs5, Fs5, Fs5, G5, E5, D5, E5, R, //15 I have always been so different, normal rules did not apply
 G4, C5, D5, D5, R, E5, E5, F5, F5, R, D5, F5, G5, G5, A5, A5, //16 Is this the day? Are you the way? I finally find out why
-C6, B5, B5, G5, G5, E5, D5, D5, A5, E5, Fs5, G5, G5, D6, R, G5, A5, B5, A5, //17 Show yourself, I'm no longer trembling Here I am I've come so far
+C6, B5, B5, G5, G5, E5, D5, D5, A5, E5, Fs5, G5, G5, D6, G5, A5, B5, A5, //17 Show yourself, I'm no longer trembling Here I am I've come so far
 E5, D5, E5, G5, E5, G5, A5, G5, E5, G5, A5, B5, D6, C6, B5, A5, //18 You are the answer I waited for all of my life
-C6, C6, B5, B5, A5, G5, G5, G5, G5, A5, G5, B5, //19 Oh, show yourself, let me see who you are
+C6, C6, B5, B5, A5, G5, G5, G5, G5, Fs5, G5, B5, //19 Oh, show yourself, let me see who you are
 A5, G5, Fs5, D5, A5, G5, Fs5, D5, A5, G5, Fs5, E5, E5, Fs5, G5, G5, B5, G5, //20 Come to me now, open your door, don't make me wait, one moment more, oh
 A5, G5, Fs5, G5, Fs5, A5, G5, Fs5, G5, Fs5, E5, D5, A5, G5, Fs5, E5, E5, Fs5, G5, G5, A5, B5, R, //21 Come to me now, open your door, don't make me wait, one moment more
-Fs5, Gs5, As5, Ds5, Fs5, Ds5, Gs5, Ds6, D6, Ds6, B5, //22 Where the north wind meets the sea (Ah-ah ah-ah)
+Fs5, Gs5, As5, Ds5, Fs5, Ds5, Gs5, Ef6, D6, Ds6, Cf6, //22 Where the north wind meets the sea (Ah-ah ah-ah)
 Gs5, Cs6, Ds6, Fs5, Fs6, F6, Fs6, Ds6, Cs6, B5, As5, Gs5, As5, F5, Fs5, Cs5, B4, Cs5, //23 There's a river (Ah-ah ah-ah) Full of memory (Full of memory)
 B5, As5, Gs5, Ds5, Ds5, F5, Fs5, As5, B5, Cs6, R, //24 Sleep my darling, safe and sound (I am found)
 Cs6, C6, C6, R, Gs5, Gs5, F5, Ds5, Ds5, As5, Cs6, C6, C6, R, C6, C6, Cs6, C6, C6, As5, //25 Show yourself, step into your power Grow yourself into something new
 C6, As5, Gs5, Gs5, As5, C6, C6, As5, Gs5, Gs5, As5, C6, C6, Cs6, C6, As5, //26 You are the one you've been waiting for (All of my life)
-Cs6, Cs6, C6, C6, As6, Gs6, //27 Oh, show yourself
-Gs5, G5, Gs5, G5, F4, G4, F4, Gs4, As5, Gs5, As5, G5, Gs4, As4, Gs4, C5, //28 Ah-ah ah-ah (Ah-ah ah-ah) Ah-ah ah-ah (Ah-ah ah-ah)
+Cs6, Cs6, C6, C6, As5, Gs5, //27 Oh, show yourself
+Af5, G5, Af5, F5, F4, G4, F4, Gs4, C6, Bf5, C6, Af5, Gs4, As4, Gs4, C5, //28 Ah-ah ah-ah (Ah-ah ah-ah) Ah-ah ah-ah (Ah-ah ah-ah)
 Ds6, D6, Ds6, C6, R //29 Ah-ah ah-ah
 };
 
@@ -66,18 +66,18 @@ t8, t16, t16, t4, t8, t8, t4d+t4, t8, t8, t4_3, t4_3, t4_3, t2d, t8, t8+t2d+t8, 
 t8, t8, t8, t8, t16, t16+t8, t4d+t4d, t8, t4_3, t4_3, t4_3, t4, t8, t4, t4, t8, t2d, //14
 t8, t8, t4, t8, t8+t4, t8, t8+t8, t4d+t4, t8, t8, t4, t8, t8+t8, t4, t8+t2d, t8, //15
 t8, t4, t8, t8+t4, t8, t8, t4, t8, t8+t4, t8, t8, t4, t8, t8+t8, t4, t8+t1, //16
-t8, t8, t2d+t4, t8, t8+t8, t8, t8, t8+t8, t8+t2d, t4d, t4d, t4+t8, t8, t8, t8, t4, t4+t8, t8, t2d, //17
+t8, t8, t2d+t4, t8, t8+t8, t8, t8, t8+t8, t8+t2d, t4d, t4d, t4+t8, t8, t4, t4, t4+t8, t8, t2d, //17
 t8, t8, t8, t8+t8, t8, t8, t8+t8, t8, t4, t4_3, t4_3, t4_3+t4, t4+t4d, t16, t16, t2, //18
 t2, t8, t8, t2, t8, t8+t4, t8, t8, t4_3, t4_3, t4_3+t4, t2d+t1, //19
 t4d, t4d, t4, t1, t4d, t4d, t4, t1, t4d, t4d, t4, t1, t4d, t4d, t4, t2d, t8, t8, //20
 t4d, t4d, t4, t8, t8+t2d, t4d, t4d, t4, t8, t8+t8, t16, t16+t2, t4d, t4d, t4, t1, t4d, t4d, t4, t4d, t4d, t4+t1+t1, t1+t2, //21
 t4, t4, t4, (t4d+t4)*2, t8d*2, t8, t2-t8, t8, t8d*2, t8, t2, //22
 t4d, t8, t4, t8, t8, t4d, t8, t2, t4d, t8, t8, t8, t4, t4d, t8, t8, t8, t4, //23
-t4d, t8, t4, t4, t4, t4+t8, t4d+t4, t8, t8+t8, t4d+t2, t2, //24
+t4d, t8, t4, t4, t4, t4+t4, t2d+t2, t4, t4+t4, t2d+t1, t2, //24
 t4, t4, t2, t4, t8, t8+t8, t8, t8, t8+t8, t8+t2d, t4, t4, t2, t4, t8, t8, t4, t4, t8, t8+t2d, //25
 t8, t8, t8, t8+t8, t8, t8, t8+t8, t8, t4, t4_3, t4_3, t4_3+t4, t4+t4d, t16, t16, t2, //26
 t2, t4, t8, t8+t4, t8, t8+t1+t4, //27
-t8, t8+t4, t8, t8+t4, t8, t8+t4, t8, t8+4, t8, t8+t4, t8, t8+t4, t8, t8+4, t8, t8+t2d, //28
+t8, t8+t4, t8, t8+t4, t8, t8+t4, t8, t8+t4, t8, t8+t4, t8, t8+t4, t8, t8+t4, t8, t8+t2d, //28
 t4, t2, t2, t1+t1, t1+t1 //29
 };
 
